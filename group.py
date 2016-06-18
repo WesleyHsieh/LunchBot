e8cb373722eec1b9b7c228306be10a8bcd345537
Wesley Hsieh <iamwesleyhsieh@gmail.com>
2016-06-17 23:04:28 -0700

    Added init/group files

---
+++ group.py
@@ -0,0 +1,11 @@
+class Group:
+    def __init__(self):
+        pass
+        # self.members = []
+        # self.recipes = []
+        # self.phase = None
+    def add_member(self):
+        pass
+    def add_recipe(self):
+        pass
+
