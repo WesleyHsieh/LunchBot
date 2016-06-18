061aebc452af0b1e9804d15f5ff7e125bd1dd57a
Varsha Kishore <varshakishore@Varshas-MacBook-Pro.local>
2016-06-17 23:15:54 -0700

    added a file

---
+++ recipe.py
@@ -0,0 +1,11 @@
+class recipe:
+	def __init__(self, name, details, cost, userVotes):
+      self.name = name
+      self.cost = cost
+      userVotes = []
+
+  	def userVoted(self, username):
+  		self.userVotes.append(username)
+
+  	def numberVotes(self):
+  		return len(userVotes)
