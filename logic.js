761e61ec039c184cf207c5a68a8ed9c2df087743
Varsha Kishore <varshakishore@Varshas-MacBook-Pro.local>
2016-06-18 03:31:20 -0700

    java script

---
+++ logic.js
@@ -0,0 +1,14 @@
+var recipe = {name: "", cost: 0, details: "", userVotes: [], link: ""};
+var updateVotes = function(person) {
+	var flag = true;
+	for (var i = 0; i < userVotes.length; i++) {
+		if (person.userid === userVotes[i].userid) {
+			flag = false
+		}
+	}
+	if (flag) {
+	recipe.userVotes = recipe.userVotes + [person];
+}
+}
+var recipeList = [];
+var person = {userid: "", groups: [], recipes: []};
