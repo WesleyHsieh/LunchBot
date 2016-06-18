d59176d9804952c41933e878b65d328d132a41c7
david siah <dsiah@villanova.edu>
2016-06-18 09:29:55 -0700

    diff recipes

--- express.js
+++ express.js
@@ -47,9 +47,37 @@ app.get('/room/:id', function(req, res) {
   var mac = RecipeLib.Recipe("Mac n Cheese");
   mac.details = "macaroni and cheese, ya dig?";
   mac.link = "http://cdn1.tmbi.com/TOH/Images/Photos/37/300x300/exps41117_ESC1801517D82.jpg";
+
+  var mac = RecipeLib.Recipe("Mac n Cheese");
+  mac.details = {"uncooked elbow macaroni": "8 ounces",
+                 "shredded Sharp Cheddar cheese": "2 cups",
+                 "grated Parmesan cheese": "0.5 cup",
+                 "milk": "3 cups",
+                 "butter": "0.25 cups",
+                 "all-purpose flour": "2.5 tablespoons",
+                 "bread crumbs": "2 tablespoons",
+                 "paprika": "1 pinch",
+                 "step1": "Cook macaroni according to package directions, drain",
+                 "step2": "In a saucepan, melt butter over medium heat. Stir in flour and add milk and cheeses",
+                 "step3": "melt butter and add breadcrumbs. Spread over mac and cheese and cover. Sprinkle paprika",
+                 "step4": "Bake at 350 degrees for 30 minutes."};
+  var caprese = RecipeLib.Recipe("Caprese Salad");
+  caprese.details = {"vine-ripe tomatoes": "3 cut into .25-inch thick slices",
+                     "fresh mozzarella": "1 pound cut into 0.25-inch thick slices",
+                     "fresh basil": "20-30 leaves",
+                     "extra-virgin olive oil": "for drizzling",
+                     "coarse salt and pepper": "for taste",
+                     "step1": "Layer alternating slices of tomato and mozzarella adding a basil leaf between each, on a large, shallow platter.",
+                     "step2": "Drizzle the salad with extra virgin olive oil and season with salt and pepper, to taste"};
+  var smoothie = RecipeLib.Recipe("Avocado and Spinach Smoothie");
+  smoothie.details = {"apple juice": "1.5 cups",
+                      "stemmed and chopped spinach": "2 cups",
+                      "unpeeled, cored, chopped apple": "1",
+                      "chopped avocado": "0.5",
+                      "step1": "Combine the apple juice, spinach, apple, and avoacdo in a blender and puree until smooth, about 1 minute, adding water to reach desired consistency"};
   var locals = {
     room_id: req.params.id,
-    recipes: [ mac, mac , mac ]
+    recipes: [ mac, caprese , smoothie ]
   };
   var html = pug.renderFile(path.join(__dirname, 'templates', 'vote.pug'), locals);
   res.send(html);
